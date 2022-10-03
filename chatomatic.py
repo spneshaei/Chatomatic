1853e364b812c646b16627ebee34b5b087f4e034
Seyed Parsa Neshaei <seyyedparsa@outlook.com>
2022-10-03 14:29:17 +0330

    Added similarity finding

--- chatomatic.py
+++ chatomatic.py
@@ -2,6 +2,8 @@
 
 from qa_database import *
 import random
+from rank_bm25 import *
+import numpy as np
 
 class Chatomatic:
     random_generator = random.Random()
@@ -18,15 +20,22 @@ class Chatomatic:
                 return self.random_generator.choice(qa.answers)
         return None
 
+    def find_most_similar_question(self, question): # From https://www.analyticsvidhya.com/blog/2021/05/build-your-own-nlp-based-search-engine-using-bm25/
+        tokenized_corpus = [doc.title.split(" ") for doc in self.qa_database.questions]
+        bm25 = BM25Okapi(tokenized_corpus)
+        tokenized_query = question.split(" ")
+        doc_scores = bm25.get_scores(tokenized_query)
+        doc_scores = list(doc_scores)
+        return self.qa_database.questions[doc_scores.index(max(doc_scores))]
+
     def answer(self, question):
         question = question.lower()
         answer = self.find_answer_to_question(question)
         if answer is None:
-            return "I don't know" # TODO: Use similarity to find the best answer
+            answer = self.random_generator.choice(self.find_most_similar_question(question).answers) # TODO: GAN
         return answer
 
 
 chatomatic = Chatomatic("test.yml")
 print(chatomatic.answer("great, thanks"))
-print(chatomatic.answer("greaT, thanks"))
-print(chatomatic.answer("Great, thanks"))
+print(chatomatic.answer("Great thakss"))
